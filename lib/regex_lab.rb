a2e133b1e48f99871f57f1ddb771525b9e21099f
Chris Franklin <cf73021@gmail.com>
2018-08-01 15:32:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.match(/\A[aeiou]/) != "[]"
     return true
-  elsif word.match(/^[^aeiou\W]/i) != "[]"
+  elsif word.match(/\A[aeiou]/) == "[]"
     return false
   end
 end
