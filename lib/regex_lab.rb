a45d4252ad4e0370fc5c67108c66d64c30a5db5d
Chris Franklin <cf73021@gmail.com>
2018-08-01 15:29:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,8 @@
 def starts_with_a_vowel?(word)
   if word.match(/\A[aeiou]/) != "[]"
-        return true
-  else return false
+    return true
+  elsif word.match(/\A[aeiou]/) != "[]"
+    return false
   end
 end
 
